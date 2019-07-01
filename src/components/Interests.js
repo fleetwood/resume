2cbc44fb51661e0efdbb67dd67502e3d87e732a6
Fleetwood <wizening@gmail.com>
2019-07-01 09:49:35 -0500

    Adding backgrounds to interests, correcting experience dates.

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Section from './Section';
 
-const item = (title, content) => (
-    <div className="m-1 p-3 col-sm-5 col-md-3 border rounded interest-item">
+const item = (title, content, img) => (
+    <div className="m-3 p-3 col-sm-5 col-md-3 border rounded interest-item">
         <h5>{title}</h5>
         <p>{content}</p>
+        <div className="item-bg w-100 h-100" style={{backgroundImage:`url(./../img/${img})`}}></div>
     </div>
 );
 
@@ -12,12 +13,13 @@ const Interests = {
         title: 'Interests',
         content: 
             <div className="w-100 d-flex align-items-start align-content-stretch flex-wrap">
-                {item('Coding','For work and for fun.')}
-                {item('Writing','My lady and I are writing a romance novel.')}
-                {item('Tattooing','We tattoo, too.')}
-                {item('Music','I fake guitar pretty well.')}
-                {item('Illustration','Been published a few times.')}
-                {item('Philosophy','Ethical quandaries and Gestaltzerfalls.')}
+                {item('Coding','For work and for fun.', 'coding.jpg')}
+                {item('Writing','My lady and I are writing a romance novel.','writing.jpg')}
+                {item('Tattooing','We tattoo, too.','tattoo.jpg')}
+                {item('Music','I fake guitar pretty well.','music.jpg')}
+                {item('Illustration','Been published a few times.','illustration.jpg')}
+                {item('Feminism','Affects 51% of us directly, 49% indirectly.','feminism.jpg')}
+                {item('Philosophy','Ethical quandaries and Gestaltzerfalls.','philosophy.jpg')}
             </div>
 }
 
